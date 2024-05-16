eef74bbec2b350f4867ece0bc3074a375fd1339b
Luis Palacios <lpalacios1119@gmail.com>
2024-05-15 22:31:35 -0600

    first commit

---
+++ Clases/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.Clases
+{
+    public class Alumno
+    {
+        public string NombreAlumno { get; private set; }
+        public string NumeroCuenta { get; private set; }
+        public string Email { get; private set; }
+
+        public void SetDatosAlumno(string nombre, string numeroCuenta, string email)
+        {
+            NombreAlumno = nombre;
+            NumeroCuenta = numeroCuenta;
+            Email = email;
+        }
+    }
+}
